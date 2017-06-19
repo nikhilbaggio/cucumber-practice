f8dbe4c7667ab8a09c165f99811d7fa54aab1b4f
Nikhil <nikhilbaggio@gmail.com>
2017-06-19 22:38:51 +0800

    Intial commit

---
+++ src/main/java/accenture/cucumber/cucumber_practice/App.java
@@ -0,0 +1,13 @@
+package accenture.cucumber.cucumber_practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
